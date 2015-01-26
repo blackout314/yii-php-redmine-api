05338ccbf6ab9b926740a4d2dd7783f4c4d579e2
Carlo Denaro <blackout_lair@yahoo.com>
2015-01-26 22:03:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# yii-php-redmine-api
---
+++ phpRedmineApi.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'php-redmine-api/autoload.php';
+
+/**
+ * Yii PHP Redmine API
+ * @author Carlo Denaro <me at carlodenaro dot com>
+ * Website: http://github.com/blackout314/yii-php-redmine-api
+ */
+class phpRedmineApi {
+	private $client;
+
+    /**
+     * @var url    url of redmine
+     * @var api    apiKey
+     */
+	public function __construct ($url, $api)
+	{
+		$this->client = new Redmine\Client($url, $api);
+	}
+	/**
+	 * @var e      type of entity
+	 */
+	public function get ($e)
+	{
+		return $this->client->api($e)->all();
+	}
+}
+
+?>
